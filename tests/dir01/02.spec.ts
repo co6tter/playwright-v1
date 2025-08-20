969fee0b4a861cecdf8aff9be4e0159e34401228
co6tter <co6tter@gmail.com>
2025-08-21 00:33:43 +0900

    feat: add dirs

--- tests/dir01/02.spec.ts
+++ tests/dir01/02.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
+  console.log('dir01/02 has title');
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
@@ -8,11 +9,14 @@ test('has title', async ({ page }) => {
 });
 
 test('get started link', async ({ page }) => {
+  console.log('dir01/02 get started link');
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(
+    page.getByRole('heading', { name: 'Installation' })
+  ).toBeVisible();
 });
